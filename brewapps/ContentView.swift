b271cc0a505c375fd5500ca6ad458574758e554e
Sagar Rai <sagarrai9893@gmail.com>
2026-01-19 21:09:02 +0530

    Initial Commit

---
+++ brewapps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  brewapps
+//
+//  Created by Apple on 19/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ brewapps/brewappsApp.swift
@@ -0,0 +1,17 @@
+//
+//  brewappsApp.swift
+//  brewapps
+//
+//  Created by Apple on 19/01/26.
+//
+
+import SwiftUI
+
+@main
+struct brewappsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
